37ef4ac6d606abe7dae951895b79063b8e5a3628
SlavikLiabin <r9514633542@gmail.com>
2024-04-14 21:27:07 +0500

    'destinationsstart'

---
+++ src/components/destinations/Destinations.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './DestinationsStyles.css';
+import BoraBora from '../../assets/borabora.jpg'
+import BoraBora2 from '../../assets/borabora2.jpg'
+import Maldives from '../../assets/maldives.jpg'
+import Maldives2 from '../../assets/maldives2.jpg'
+import KeyWest from '../../assets/keywest.jpg'
+
+
+function Destinations() {
+  return (
+    <div className='destinations'>
+      <div className="container">
+        <h1>All inclusive Resorts</h1>
+        <p>On the Caribbean's Best Beaches</p>
+        <div className="img-container">
+            <img src={BoraBora} alt="/" />
+            <img src={BoraBora2} alt="/" />
+            <img src={Maldives} alt="/" />
+            <img src={Maldives2} alt="/" />
+            <img src={KeyWest} alt="/" />
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Destinations
